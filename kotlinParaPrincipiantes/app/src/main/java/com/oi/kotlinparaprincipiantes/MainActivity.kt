5495601312c6600f123c4c3ca863629784711a2c
Luis <luisaguadodiego1993@gmail.com>
2024-11-13 13:57:39 +0100

    aa

--- kotlinParaPrincipiantes/app/src/main/java/com/oi/kotlinparaprincipiantes/MainActivity.kt
+++ kotlinParaPrincipiantes/app/src/main/java/com/oi/kotlinparaprincipiantes/MainActivity.kt
@@ -18,7 +18,13 @@ class MainActivity : AppCompatActivity() {
         //tiposDeDatos()
 
         //clase3
-        sentenciaIf()
+      //  sentenciaIf()
+
+        //clase4
+       // sentenciaWhen()
+
+        //clase5
+        arrays()
     }
     /*
     Aqui vamos a hablar de variables y constantes
@@ -94,7 +100,9 @@ class MainActivity : AppCompatActivity() {
         println(myBool && myBool2)
 
     }
-    /**/
+    /*
+    * Sentencia if
+    * */
     private fun sentenciaIf(){
         val myNumber = 12
 
@@ -106,13 +114,116 @@ class MainActivity : AppCompatActivity() {
         // == igualdad
         // ! = desigualdad
 
-        if (myNumber < 10){
+        //Operadores logicos
+        // && operador y
+        // || operador o
+        // ! operador no
+
+        if ((myNumber < 10 && myNumber > 5) || myNumber == 53){
             //Sentencia if
             println("$myNumber es menor o igual que 10")
+        } else if(myNumber == 60){
+            println("El numero es igual a 60")
         } else {
             //Sentencia else
             println("$myNumber es mayor que 10")
         }
+
+    }
+    /*
+    * sentencia when
+    * */
+    private fun sentenciaWhen(){
+
+        //when con string
+        val country = "España"
+
+        when (country){
+            "España" , "mexico" , "Argentina" ->{
+                println("El idioma es español")
+            }
+             "Euuu" ->{
+                 println("El idioma es ingles")
+             }
+            else ->{
+                println("oi")
+            }
+        }
+        //when con int
+        val age = 1
+
+        when (age) {
+            0 , 1 , 2 ->{
+                println("Es un bebe")
+            }
+            in 3 .. 10 ->{
+                println("Es un niño")
+            }
+            else ->{
+                println("oi")
+            }
+        }
+    }
+
+    private fun arrays(){
+        val name = "Brais"
+        val surname = "oi"
+        val company = "oioi"
+        val age = "32"
+
+        //Creacion de un Array
+
+        val myArray : ArrayList<String> = arrayListOf<String>()
+
+        //Añadir datos de uno en uno
+
+        myArray.add(name)
+        myArray.add(surname)
+        myArray.add(company)
+        myArray.add(age)
+        //myArray.add(age)
+        //myArray.add(age)
+
+        println(myArray)
+
+        //añadir un conjunto de datos
+
+        myArray.addAll(listOf("Hola", "Bienvenidos al tutorial"))
+
+        println(myArray)
+
+        //acceso a datos
+
+        val myCompany : String = myArray[2]
+        println(myCompany)
+
+        //modificacion de datos
+
+        myArray[5] = "Suscribete ..."
+        println(myArray)
+
+        //Eliminar datos
+        myArray.removeAt(4)
+
+        println(myArray)
+
+        // Recorrer datos
+
+        myArray.forEach{
+            println(it)
+        }
+
+        //otras operaciones
+
+        println(myArray.count())
+
+        myArray.clear()
+
+        println(myArray.count())
+
+        myArray.first()
+        myArray.last()
+
 
     }
 }
